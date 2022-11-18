c719287f9c287f52507d5e5d20205bee6cb418e4
shilvisahu <shilvisahu@gmail.com>
2022-11-18 15:49:24 +0530

    Added removedProducts list in cart

--- src/Main.java
+++ src/Main.java
@@ -4,32 +4,22 @@ import domain.Price;
 import domain.Product;
 
 import java.util.Currency;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
         Cart cart = new Cart();
 
-        /*Product applePencil = new Product("Apple Pencil");
-        cart.addLineItem(new LineItem(applePencil, 2));
-*/
+        cart.listOfItems();
+
+        HashMap<String, Price> productPrice = new HashMap<>();
+
+        productPrice.put("Sony headphones", new Price(10));
+
         Product sonyHeadphones = new Product("Sony headphones", new Price(5));
         cart.addLineItem(new LineItem(sonyHeadphones, 1));
 
-        /*cart.listOfItems();
 
-        cart.removeItem(applePencil);
-
-        System.out.println("List after removing items: ");*/
-
-        cart.listOfItems();
-
-        System.out.println("removed items:" +cart.getRemovedProducts());
-
-        Cart cart2 = new Cart();
-        Product sonyHeadphones1 = new Product("Sony headphones", new Price(5));
-        cart.addLineItem(new LineItem(sonyHeadphones1, 1));
-
-        System.out.println(cart.equals(cart2));
 
 
     }
